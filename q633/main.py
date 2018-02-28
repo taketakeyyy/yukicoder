dabf26b674fe4f98d459ce67dd39222b729666fe
takey <taketakeyyy@gmail.com>
2018-02-28 18:29:42 +0900

    add: q633

---
+++ q633/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    n = int(input())
+    a = []
+    for i in range(n-1):
+        a.append(int(input()))
+    b_c = []
+    for i in range(n):
+        inputs = input()split()
+        b_c.append([int(inputs[0]), int(inputs[1])])
+    ans = 0
+
+    
