b10ff8d232cf890405ec227389fa7da9277053a1
takey <taketakeyyy@gmail.com>
2018-03-02 15:15:06 +0900

    solved: q9002

---
+++ q9002/main.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+	N = int(input())
+	for i in range(1, N+1):
+		if i%3==0 and i%5==0:
+			print("FizzBuzz")
+		elif i%3==0:
+			print("Fizz")
+		elif i%5==0:
+			print("Buzz")
+		else:
+			print(i)
