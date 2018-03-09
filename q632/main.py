cf99379f4611105bd3c1d8b825f3f80bd34fe508
takey <taketakeyyy@gmail.com>
2018-03-10 00:23:20 +0900

    solved: q632

---
+++ q632/main.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+if __name__ == "__main__":
+    C = input().split()
+    if C[1] == "2":
+        print("4")
+    elif C[1] == "3":
+        print("1")
+    else:
+        print("14")
