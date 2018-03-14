1bcc6f4b47df485a5535d9fc4e561204bf09eece
takey <taketakeyyy@gmail.com>
2018-03-14 23:37:56 +0900

    solved: q558

---
+++ q558/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string s;
+  cin >> s;
+  int pos;
+  pos = s.find("575")
+  if(pos != std::string::npos){
+    cout << "YES\n"
+  }
+  else{
+    cout << "NO\n"
+  }
+}
